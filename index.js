74f9941608c3f8215503285d94c617ee08771553
Victor Estrada <vic4187@gmail.com>
2024-07-14 00:40:01 -0500

    Updated styling using SCSS/SASS

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var context = canvas.getContext('2d');
 var paused = false;
 var grid = 16;
 var count = 0;
+var frameSkip = 10;  // Increase this value to slow down the snake
 
 function resetSnake() {
     var snake = {
@@ -17,40 +18,40 @@ function resetSnake() {
     };
     return snake;
 }
+
 var snake = resetSnake();
 var score = 0;
 var hiscore = 0;
-var apple = {x: 320, y: 320};
+var apple = { x: 320, y: 320 };
 
 const getRandomInt = (mn, mx) => Math.floor(Math.random() * (mx - mn)) + mn;
 
 function gameLoop() {
-    
     requestAnimationFrame(gameLoop);
-    if (++count < 4) return;
-    if (paused) throwError();
+    if (++count < frameSkip) return;  // Use frameSkip variable here
+    if (paused) return;
     count = 0;
     context.clearRect(0, 0, canvas.width, canvas.height);
 
     snake.x += snake.dx;
     snake.y += snake.dy;
-    
+
     if (snake.x < 0) snake.x = canvas.width - grid;
     else if (snake.x >= canvas.width) snake.x = 0;
 
     if (snake.y < 0) snake.y = canvas.height - grid;
     else if (snake.y >= canvas.width) snake.y = 0;
 
-    snake.cells.unshift({x: snake.x, y: snake.y});
+    snake.cells.unshift({ x: snake.x, y: snake.y });
 
     if (snake.cells.length > snake.maxCells) snake.cells.pop();
 
-    context.fillStyle = 'red';
-    context.fillRect(apple.x, apple.y, grid+1, grid+1);
+    context.fillStyle = 'green';
+    context.fillRect(apple.x, apple.y, grid + 1, grid + 1);
 
-    context.fillStyle = 'blue';
-    snake.cells.forEach(function(cell, index) {
-        context.fillRect(cell.x, cell.y, grid+1, grid+1)
+    context.fillStyle = 'red';
+    snake.cells.forEach(function (cell, index) {
+        context.fillRect(cell.x, cell.y, grid + 1, grid + 1);
         if (cell.x === apple.x && cell.y == apple.y) {
             snake.maxCells++;
             scoreDisplayElem.innerHTML = ++score;
@@ -74,7 +75,7 @@ function gameLoop() {
     });
 }
 
-document.addEventListener('keydown', function(e) {
+document.addEventListener('keydown', function (e) {
     if (e.which === 37 && snake.dx === 0) {
         snake.dx = -grid;
         snake.dy = 0;
@@ -119,7 +120,7 @@ function down() {
     if (snake.dy === 0) {
         snake.dy = grid;
         snake.dx = 0;
-    }  
+    }
 };
 function pause() {
     paused = !paused;
